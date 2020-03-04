7486207e6d3012bc06dc09d7e910e26146a508af
eclaszlo <59945343+eclaszlo@users.noreply.github.com>
2020-03-04 16:32:51 -0500

    Assets, Scenes, and Scripts folders

---
+++ Scripts/Bullets.gd
@@ -0,0 +1,13 @@
+extends Node2D
+var ready = true
+
+func _ready():
+	pass
+
+func fire(blast):
+	if ready:
+		add_child(blast)
+		ready = false	
+		$Timer.start()
+func _on_Timer_timeout():
+	 ready = true
